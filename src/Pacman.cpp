628af427db975f8be73ba29af693b19984c6acd7
Gabriel Brown <gbrown3@macalester.edu>
2018-10-22 10:03:06 -0500

    Removed groundplane, added pacman class and header files

---
+++ src/Pacman.cpp
@@ -0,0 +1,8 @@
+//
+//  Pacman.cpp
+//  Pac-Sphere
+//
+//  Created by Gabriel Brown on 10/22/18.
+//
+
+#include "Pacman.hpp"
---
+++ src/Pacman.hpp
@@ -0,0 +1,13 @@
+//
+//  Pacman.hpp
+//  Pac-Sphere
+//
+//  Created by Gabriel Brown on 10/22/18.
+//
+
+#ifndef Pacman_hpp
+#define Pacman_hpp
+
+#include <stdio.h>
+
+#endif /* Pacman_hpp */
