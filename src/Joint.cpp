50f2529872f495bbd4213a7b92bfa75872271bd2
Gabriel Brown <gbrown3@macalester.edu>
2018-11-15 16:44:27 -0600

    Initial empty joint class

---
+++ src/Joint.cpp
@@ -0,0 +1,8 @@
+//
+//  Joint.cpp
+//  Pac-Sphere
+//
+//  Created by Gabriel Brown on 11/15/18.
+//
+
+#include "Joint.h"
---
+++ src/Joint.h
@@ -0,0 +1,13 @@
+//
+//  Joint.hpp
+//  Pac-Sphere
+//
+//  Created by Gabriel Brown on 11/15/18.
+//
+
+#ifndef Joint_hpp
+#define Joint_hpp
+
+#include <stdio.h>
+
+#endif /* Joint_hpp */
