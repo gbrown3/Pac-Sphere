da040207348ed2c89c2b982765d61c895024670c
Gabriel Brown <gbrown3@macalester.edu>
2018-11-16 09:37:32 -0600

    First attempt at joint class

--- src/Joint.cpp
+++ src/Joint.cpp
@@ -6,3 +6,42 @@
 //
 
 #include "Joint.h"
+
+using namespace basicgraphics;
+using namespace std;
+using namespace glm;
+
+namespace pacsphere {
+    
+    // Root joint
+    Joint::Joint(vec3 localPosition) {
+        
+        _localPosition = localPosition;
+        _childJoints = vector<shared_ptr<Joint>>();
+        _jointSphere.reset(new Sphere(localPosition, SPHERE_RADIUS, vec4(1, 0, 0, 1)));
+    }
+    
+    // Normal Joint
+    Joint::Joint(std::shared_ptr<Joint> parentJoint, glm::vec3 localPosition) {
+        
+        _parentJoint = parentJoint;
+        _localPosition = localPosition;
+        _childJoints = vector<shared_ptr<Joint>>();
+        _jointSphere.reset(new Sphere(localPosition, SPHERE_RADIUS, vec4(1, 0, 0, 1)));
+        
+        _parentJoint->addChild(shared_from_this());
+        
+    }
+    
+    
+    void Joint::addChild(std::shared_ptr<Joint> childJoint) {
+        
+        _childJoints.push_back(childJoint);
+    }
+    
+    
+    void Joint::draw(GLSLProgram &shader, const mat4 &modelMatrix) {
+        
+        _jointSphere->draw(shader, modelMatrix);
+    }
+}
--- src/Joint.h
+++ src/Joint.h
@@ -1,13 +1,49 @@
 //
-//  Joint.hpp
+//  Joint.h
 //  Pac-Sphere
 //
 //  Created by Gabriel Brown on 11/15/18.
 //
 
-#ifndef Joint_hpp
-#define Joint_hpp
+#ifndef Joint_h
+#define Joint_h
 
 #include <stdio.h>
+#include <glm/glm.hpp>
+#include <BasicGraphics.h>
 
-#endif /* Joint_hpp */
+namespace pacsphere {
+    
+    /**
+     * Represents a joint to be used for rigging, along with its parent Joint,
+     * a list of child joints, and a method for drawing a graphical representation
+     */
+    class Joint : public std::enable_shared_from_this<Joint> {
+        
+    public:
+        
+        static constexpr float SPHERE_RADIUS = 0.5f;
+        
+        // Constuctor for root joint, initialized with coordinate in model space (not world)
+        Joint(glm::vec3 localPosition);
+        
+        // Constructor for all other joints
+        Joint(std::shared_ptr<Joint> parentJoint, glm::vec3 localPosition);
+        
+        // Adds joint to list of children
+        void addChild(std::shared_ptr<Joint> childJoint);
+        
+        // Draws basicgraphics sphere to show precisely where the joint is
+        void draw(basicgraphics::GLSLProgram &shader, const glm::mat4 &modelMatrix);
+        
+    private:
+        
+        glm::vec3 _localPosition;
+        std::shared_ptr<pacsphere::Joint> _parentJoint;
+        std::vector<std::shared_ptr<Joint>> _childJoints;
+        
+        std::unique_ptr<basicgraphics::Sphere> _jointSphere;
+    };
+}
+
+#endif /* Joint_h */
